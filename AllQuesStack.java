d01227958acf85105dc7b920bb9d88f41a7cfc52
Shivam-shiva <shivamsingh.letsmail@gmail.com>
2025-04-12 21:48:37 +0530

    Code for check reduntant bracket

--- AllQuesStack.java
+++ AllQuesStack.java
@@ -13,7 +13,7 @@ public class AllQuesStack {
 //		st.push(7);st.push(9);
 //		st.pop();st.pop();st.pop();st.pop();st.pop();st.pop();st.pop();st.pop();
 //		System.out.println(st.pop()+" "+st.size());
-		System.out.print(balancedParanthesis(s.next()));
+		System.out.print(redundantBracket(s.next()));
 
 	}
 	
@@ -44,8 +44,37 @@ public class AllQuesStack {
 		
 		return stack.size()==0;
 	}
+	
+	/*For a given expression in the form of a string, find if there exist any redundant brackets or not. It is given that the expression contains only rounded brackets or parenthesis and the input expression will always be balanced.
+
+	A pair of the bracket is said to be redundant when a sub-expression is surrounded by unnecessary or needless brackets.
+
+	Example:
+	Expression: (a+b)+c
+	Since there are no needless brackets, hence, the output must be 'false'.
+
+	Expression: ((a+b))
+	The expression can be reduced to (a+b). Hence the expression has redundant brackets and the output will be 'true'.*/
+	
+	public static boolean redundantBracket(String str) {
+		Stack<Character> stack = new Stack<>();
+
+		for(int i=0;i<str.length();i++) {
+			Character ch= str.charAt(i);
+			if(ch=='(')
+				stack.push('(');
+			 else if(stack.size()!=0 && stack.peek()=='('&& (ch=='+' || ch=='-'||ch=='*'|| ch=='/')) 
+				stack.push(ch);
+			 else if(ch==')'&& stack.size()!=0 && (stack.peek()=='+' || stack.peek()=='-' ||stack.peek()=='*' || stack.peek()=='/'))
+				 while(stack.pop()!='(');
+		}
+		return stack.size()!=0;
+	}
 
 }
+
+
+
 
 
 
