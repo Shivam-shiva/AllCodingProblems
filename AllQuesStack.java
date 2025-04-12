38d28834566a61aeca28f20bc968d762b7f7bd3b
Shivam-shiva <shivamsingh.letsmail@gmail.com>
2025-04-12 18:40:27 +0530

    Implemented Stack with basic functionality like push pop etc

---
+++ AllQuesStack.java
@@ -0,0 +1,17 @@
+package allQuestions;
+
+public class AllQuesStack {
+
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		System.out.println(st.size()+" "+st.pop());
+//		st.push(6);st.push(2);st.push(4);
+//		st.push(7);st.push(9);
+//		st.push(6);st.push(2);st.push(4);
+//		st.push(7);st.push(9);
+//		st.pop();st.pop();st.pop();st.pop();st.pop();st.pop();st.pop();st.pop();
+//		System.out.println(st.pop()+" "+st.size());
+
+	}
+
+}
---
+++ Stack.java
@@ -0,0 +1,61 @@
+package allQuestions;
+
+public class Stack {
+
+	private int[] arr;
+	private int index;
+	private final int intialArraySize=5;
+	
+	public Stack() {
+		arr= new int[3];
+	}
+	
+	
+	public void push(int value) {
+		if(index==arr.length) 
+			doubleTheArray();
+			
+		arr[index]=value;
+		index++;
+	}
+	
+	public int pop() {
+		if(index==0) {
+			System.out.println("Stack is empty");
+			return -1;
+		}
+		index--;
+		if(index>=intialArraySize && index<arr.length/3)
+			reduceArraySize();
+	    return arr[index];
+	}
+	
+	public int getTop() {
+		if(index==0) {
+			System.out.println("Stack is empty");
+			return -1;
+		}
+		return arr[index-1];
+	}
+	
+	public int size() {
+		return index;
+	}
+	
+	private void doubleTheArray() {
+		int temp[] = arr;
+		arr=new int[2*arr.length];
+		for(int i=0;i<temp.length;i++)
+			arr[i]=temp[i];
+	}
+	
+	private void reduceArraySize() {
+		int temp[]=arr;
+		arr= new int[arr.length/2];
+		for(int i=0;i<arr.length;i++)
+			arr[i]=temp[i];
+	}
+}
+
+
+
