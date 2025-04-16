88f7586bcc5e7bad6ef4bc829fca4bb6f498e5aa
Shivam-shiva <shivamsingh.letsmail@gmail.com>
2025-04-16 18:29:02 +0530

    Code to find min and max in Binary Tree

--- BianryTreeUse.java
+++ BianryTreeUse.java
@@ -23,6 +23,17 @@ class DiameterInfo{
 	}
 }
 
+class Pair<T, U> {
+	T minimum;
+	U maximum;
+
+	public Pair(T minimum, U maximum) {
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+
+}
+
 public class BianryTreeUse {
 
 	public static void main(String[] args) {
@@ -173,6 +184,26 @@ public class BianryTreeUse {
 			return root;
 	}
 	
+	
+	/*For a given a Binary Tree of type integer, find and return the minimum and the maximum data values.
+
+			Return the output as an object of Pair class, which is already created.
+
+			Note:
+			All the node data will be unique and hence there will always exist a minimum and maximum node data.*/
+	
+	public static Pair<Integer, Integer> minAndMax(BinaryTreeNode<Integer> node){
+		if(node==null)
+			return new Pair<>(Integer.MAX_VALUE, Integer.MIN_VALUE);
+		
+		Pair<Integer, Integer> leftPair= minAndMax(node.left);
+		Pair<Integer, Integer> rightPair= minAndMax(node.right);
+		
+		int min= Math.min(node.data, Math.min(leftPair.minimum, rightPair.minimum));
+		int max = Math.max(node.data, Math.max(leftPair.maximum, rightPair.maximum));
+		
+		return new Pair<>(min, max);
+	}
 	
 
 }
